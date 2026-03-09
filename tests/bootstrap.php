005575021177b7a83df89cc8ac25e45d5a8bacc2
Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2026-03-09 23:44:20 +0600

    unit testing setup fixed

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@ tests_add_filter(
 	'muplugins_loaded',
 	function () {
 
-		require __DIR__ . '/../free-stock-images.php';
+		require __DIR__ . '/../plugmint-stock-images.php';
 	}
 );
 require '/wp-phpunit/includes/bootstrap.php';
